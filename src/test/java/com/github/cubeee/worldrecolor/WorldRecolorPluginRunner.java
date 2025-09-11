23cdaacb29a972e97b580a6343506b8c101aefde
cubeee <cubeee.gh@gmail.com>
2025-09-11 19:13:37 +0300

    Make plugin runner test class look less like a test class

--- src/test/java/com/github/cubeee/worldrecolor/WorldRecolorPluginRunner.java
+++ src/test/java/com/github/cubeee/worldrecolor/WorldRecolorPluginRunner.java
@@ -3,7 +3,10 @@ package com.github.cubeee.worldrecolor;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class WorldRecolorPluginTest {
+public final class WorldRecolorPluginRunner {
+	private WorldRecolorPluginRunner() {}
+
+	@SuppressWarnings({"PMD.SignatureDeclareThrowsException", "unchecked"})
 	public static void main(String[] args) throws Exception {
 		ExternalPluginManager.loadBuiltin(WorldRecolorPlugin.class);
 		RuneLite.main(args);
